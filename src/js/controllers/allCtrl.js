5313f6e0ed5f5580c04761ffdc1f9c4edd67b510
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-01-05 12:26:42 +0100

    Removed unused controllers

--- src/js/controllers/allCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('yourcall:app').controller('AllCtrl', function ($scope, questionService) {
-    questionService.getAllQuestions().success(function (questions) {
-        $scope.questions = questions;
-    });
-});
--- src/js/controllers/reportedCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('yourcall:app').controller('ReportedCtrl', function ($scope, questionService) {
-    questionService.getReportedQuestions().success(function (questions) {
-        $scope.questions = questions;
-    });
-});
