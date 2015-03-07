9f89312ba0bc06f28cdb0a01ddbe997567ac42c8
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-03-07 14:34:16 +0100

    Change app structure to facilitate testing

--- test/question.spec.js
+++ test/question.spec.js
@@ -1,42 +1,35 @@
-var mongoose       = require('mongoose'),
-    request        = require('supertest'),
-    express        = require('express'),
-    handler        = require('../lib/handlers'),
-    questionSchema = require('../lib//question').questionSchema;
+process.env.NODE_ENV = 'test';
 
-var app = require('../lib/server');
-
-var db = mongoose.createConnection('localhost', 'testdb');
-var Question = db.model('Question', questionSchema);
+var config   = require('../lib/config')
+    request  = require('supertest'),
+    app      = require('../lib/server');
+    Question = config.getModel();
 
 describe('Questions', function () {
-    var currentQuestion;
-
     beforeEach(function (done) {
         Question.create({
             title: 'Test question title',
-            url: 'urlId',
-            creator: 'creator',
-            option_1: 'req.body.option_1',
-            option_2: 'req.body.option_2',
-            isPrivate: 'req.body.isPrivate'
+            url: '12345',
+            creator: 'John Doe',
+            option_1: 'Option 1',
+            option_2: 'Option 2',
+            isPrivate: false
         }, function (err, question) {
             done();
         });
     });
-/*
+
     afterEach(function (done) {
         Question.remove({}, function () {
             done();
         });
     });
-*/
+
     it('responds with json', function (done) {
         request(app)
-            .get('/api/questions/uMqiVp')
+            .get('/api/questions/12345')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200)
-            .expect(/afsd/, done);
+            .expect(200, done);
     });
 });
