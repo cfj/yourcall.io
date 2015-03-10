bf10e526f562474340f10e9846d611d39a6156ab
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-03-10 19:45:00 +0100

    Removed old code

--- src/js/services/questionService.js
+++ src/js/services/questionService.js
@@ -25,12 +25,6 @@ angular.module('yourcall:services').factory('questionService',function ($http, $
             if (utilityService.readCookie(appService.cookieNames.OWNED_QUESTIONS)) {
                 var owned = utilityService.readCookie(appService.cookieNames.OWNED_QUESTIONS).split('|');
 
-                /*owned.forEach(function (token) {
-                    if (questionId === owned[i].split(':')[0]) {
-                        ownershipToken = owned[i].split(':')[1];
-                    }
-                });*/
-
                 for(var i = 0; i < owned.length; i++) {
                     if (questionId === owned[i].split(':')[0]) {
                         ownershipToken = owned[i].split(':')[1];
